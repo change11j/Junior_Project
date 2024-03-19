833fb2f1fb7c99d3e3b2cfe289dbbb37fc2a04f6
change11j <change11j@gmail.com>
2024-03-20 02:24:42 +0800

    差不多完成了

--- javascript/meritJs.js
+++ javascript/meritJs.js
@@ -14,30 +14,93 @@ var meritForNum = 0;
 document.addEventListener("DOMContentLoaded", () => {
   popUp.style.display = "block";
 });
+
+shopTag.addEventListener("click", () => { 
+  shop.style.display = "block";
+})
+document.addEventListener("click", function (event) {
+  // 如果点击的不是 toggleButton 或 myElement，则隐藏 myElement
+  if (
+    event.target !== shopTable &&
+    !shopTable.contains(event.target) &&
+    event.target !== shopTag
+  ) {
+    shop.style.display = "none";
+  }
+});
 //* -----------------------------------------------------------------------------
 // *                     表單 監聽 功能
 //* -------------------------------------------------------------------------
 infoForm.addEventListener("submit", (e) => {
   e.preventDefault();
-
+  
   if (foodDisc.value === "carn") {
+    tag.innerHTML = "<span>葷食者</span><br><span>因為殺生每秒自動扣去功德</span>";
     carnivore();
+    
   }
-  if (modeSelect.value === "cat") {
-    var newButton = document.createElement("button");
-    newButton.innerHTML = "貓貓";
-    newButton.id = "catButton";
-    clickArea.replaceChild(newButton, gButton);
-    catButton = newButton;
-    catButton.addEventListener("click", catMode);
+  if (foodDisc.value === "veg") {
+    tag.innerHTML =
+      "<span>素食者</span><br><span>不會扣功德，但吃飯很麻煩</span>";
+    
   }
+    if (modeSelect.value === "cat") {
+      tag.innerHTML =
+        "<span>貓貓模式</span><br><span>一隻偉大的貓咪曾經說過：“喵”</span>";
+      var newButton = document.createElement("button");
+      newButton.innerHTML = "？放生：吃魚";
+      newButton.id = "catButton";
+      clickArea.replaceChild(newButton, gButton);
+      catButton = newButton;
+      catButton.addEventListener("click", catMode);
+      catButton.addEventListener("mouseover", () => {
+        timer = setInterval(function () {
+          let isMove = Math.random() < 0.5;
+          if (isMove) {
+            let isRight = Math.random() < 0.5;
+            if (isRight) {
+              btnMoveR();
+            } else {
+              btnMoveL();
+            }
+          }
+          // 在此处执行持续触发的操作
+          console.log("持续触发");
+        }, 500); // 100毫秒为触发间隔，您可以根据需要调整
+        
+      });
+      catButton.addEventListener('mouseleave', () => {
+        clearInterval(timer);
+        
+      });
+      catButton.addEventListener("click", () => {
+        let isRight = Math.random() < 0.5;
+        if (isRight) {
+          fallRight();
+        } else {
+          fallLeft();
+        }
+      });
+    }
+
+  
+
+
   popUp.style.display = "none";
 });
 modeSelect.addEventListener("change", (e) => {
   if (modeSelect.value === "person") {
     modeDisp.style.display = "block";
+  } else { 
+    modeDisp.style.display = "none";
   }
+
 });
+function playFallAudio() {
+  const audio = document.createElement("audio");
+  audio.src = "../sounds/fall.mp3";
+  audio.play();
+}
 //* -----------------------------------------------------------------------------
 // *                     表單 監聽 功能結束
 //* -------------------------------------------------------------------------
@@ -52,9 +115,7 @@ gButton.addEventListener("click", () => {
     fallRight();
   } else {
     fallLeft();
-
   }
-  
 });
 triButton.addEventListener("click", triple);
 quintButton.addEventListener("click", quintuple);
@@ -65,10 +126,11 @@ gatlButton.addEventListener("click", () => {
 });
 forButton.addEventListener("click", () => {
   forButton.disabled = true;
-  console.log(forButton.disabled);
   meritForever();
   meritNumDisp.innerHTML = ++meritForAmt;
 });
+
+
 
 //* -----------------------------------------------------------------------------
 // *                     按鈕 監聽 功能結束
@@ -247,9 +309,15 @@ function quad(timeFraction) {
   return Math.pow(timeFraction, 2);
 }
 
-function fallRight() {
+function fallLeft() {
+  let isWho = Math.random() > 0.5;
+
   let fish = document.createElement("img");
-  fish.src = "../images/heads-downward.svg";
+  if (isWho) {
+    fish.src = "../images/heads-downward.svg";
+  } else {
+    fish.src = "../images/a--fish-heads-left.png";
+  }
   fish.className = "fish";
   fish.style.height = "100px";
   fish.style.width = "100px";
@@ -257,8 +325,9 @@ function fallRight() {
   fish.style.top = animateField.clientHeight - 100 + "px";
   // console.log(fish);
   animateField.appendChild(fish);
+  let rightBorder = animateField.clientWidth;
 
-  let xStart = Math.floor(Math.random() * 700) + 1;
+  let xStart = Math.floor((Math.random() * rightBorder) / 2) + 1;
 
   let isPlayed = false;
 
@@ -288,9 +357,14 @@ function fallRight() {
     animateField.removeChild(fish);
   }, 2000);
 }
-function fallLeft() {
+function fallRight() {
+  let isWho = Math.random() > 0.5;
   let fish = document.createElement("img");
-  fish.src = "../images/a-fish-.svg";
+  if (isWho) {
+    fish.src = "../images/a-fish-.svg";
+  } else {
+    fish.src = "../images/aFishVertical.png";
+  }
   fish.className = "fish";
   fish.style.height = "100px";
   fish.style.width = "100px";
@@ -299,11 +373,9 @@ function fallLeft() {
   // console.log(fish);
   animateField.appendChild(fish);
   let rightBorder = animateField.clientWidth;
-  console.log(animateField.clientWidth);
 
   let xStart =
-    Math.floor(Math.random() * (rightBorder / 2 + 1)) + (rightBorder / 2);
-  console.log(xStart);
+    Math.floor(Math.random() * (rightBorder / 2 + 1)) + rightBorder / 2;
 
   let isPlayed = false;
 
@@ -325,34 +397,64 @@ function fallLeft() {
     duration: 2000,
     timing: makeEaseOut(quad),
     draw: function (progress) {
-      fish.style.left = 
-
-        xStart-(progress * animateField.clientWidth/2) + "px";
+      fish.style.left =
+        xStart - (progress * animateField.clientWidth) / 2 + "px";
     },
   });
   setTimeout(() => {
     animateField.removeChild(fish);
   }, 2000);
 }
+function btnMoveR() {
+  let startR = catButton.offsetLeft + catButton.offsetWidth;
+  let endR = clickArea.offsetLeft + clickArea.offsetWidth;
+  // console.log(catButton.style.left);
+  animate({
+    duration: 4000,
+    timing: makeEaseOut(quad),
+    draw: function (progress) {
+      if (startR< endR) {
+        catButton.style.left =
+          startR + progress * 200 + catButton.offsetWidth > endR
+            ? endR - catButton.offsetWidth - progress * 200 + "px"
+            : startR + progress * 200 + "px";
+  // console.log(catButton.style.left);
+        
+      } else {
+        catButton.style.left = endR - catButton.offsetWidth - progress * 200 + "px";
+      }  
+      
+    }
+})
+}
+function btnMoveL() {
+  let startL = catButton.offsetLeft;
+  let endL = clickArea.offsetLeft;
+  animate({
+    duration: 1000,
+    timing: makeEaseOut(quad),
+    draw: function (progress) {
+      if (startL > endL) {
+        catButton.style.left =
+          startL - progress * 200 > endL
+            ?
+            startL - progress * 200 + "px" :
+            -(startL - progress * 200) + "px";
+      } else {
+        catButton.style.left = -(startL - progress * 200) + "px";
+      }
+    },
+  });
+  
+}
 
 //* -----------------------------------------------------------------------------
 // *                     動畫 控制結束
 //* -------------------------------------------------------------------------
 //* -----------------------------------------------------------------------------
-// *                     Css 控制
+// *                     商店彈窗 控制
 //* -------------------------------------------------------------------------
 
-// ! 要再修改
-// TODO 再修改
-function shopShow() {
-  if (meritAmt.innerHTML > 9) {
-    shop.classList.remove("hidden");
-    shop.classList.add("visible");
-  }
-}
 
-function playFallAudio() {
-  const audio = document.createElement("audio");
-  audio.src = "../sounds/fall.mp3";
-  audio.play();
-}
+
+
