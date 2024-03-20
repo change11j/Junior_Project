7024240f5359d0e86bac9746b7f2621c27affd05
change11j <change11j@gmail.com>
2024-03-21 01:29:53 +0800

    完成版 沒加排行榜

--- javascript/meritJs.js
+++ javascript/meritJs.js
@@ -10,14 +10,17 @@ var catButton;
 var meritForAmt = 1;
 var meritForCost = 1;
 var meritForNum = 0;
+var catTimer = 0;
 
 document.addEventListener("DOMContentLoaded", () => {
   popUp.style.display = "block";
+  // shopRemind([0, 0, 0], [288, 194, 0], new Date().getTime(), 1000)();
 });
 
-shopTag.addEventListener("click", () => { 
+
+shopTag.addEventListener("click", () => {
   shop.style.display = "block";
-})
+});
 document.addEventListener("click", function (event) {
   // 如果点击的不是 toggleButton 或 myElement，则隐藏 myElement
   if (
@@ -28,73 +31,72 @@ document.addEventListener("click", function (event) {
     shop.style.display = "none";
   }
 });
+
 //* -----------------------------------------------------------------------------
 // *                     表單 監聽 功能
 //* -------------------------------------------------------------------------
 infoForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  
+
   if (foodDisc.value === "carn") {
-    tag.innerHTML = "<span>葷食者</span><br><span>因為殺生每秒自動扣去功德</span>";
+    tag.innerHTML =
+      "<span>葷食者</span><br><span>因為殺生每秒自動扣去功德</span>";
     carnivore();
-    
   }
   if (foodDisc.value === "veg") {
     tag.innerHTML =
       "<span>素食者</span><br><span>不會扣功德，但吃飯很麻煩</span>";
-    
   }
-    if (modeSelect.value === "cat") {
-      tag.innerHTML =
-        "<span>貓貓模式</span><br><span>一隻偉大的貓咪曾經說過：“喵”</span>";
-      var newButton = document.createElement("button");
-      newButton.innerHTML = "？放生：吃魚";
-      newButton.id = "catButton";
-      clickArea.replaceChild(newButton, gButton);
-      catButton = newButton;
-      catButton.addEventListener("click", catMode);
-      catButton.addEventListener("mouseover", () => {
-        timer = setInterval(function () {
-          let isMove = Math.random() < 0.5;
-          if (isMove) {
-            let isRight = Math.random() < 0.5;
-            if (isRight) {
-              btnMoveR();
-            } else {
-              btnMoveL();
-            }
+  if (modeSelect.value === "cat") {
+    tag.innerHTML =
+      "<span>貓貓模式</span><br><span>一隻偉大的貓咪曾經說過：“喵”</span>";
+    var newButton = document.createElement("button");
+    newButton.innerHTML = "？放生：吃魚";
+    newButton.id = "catButton";
+    clickArea.replaceChild(newButton, gButton);
+    catButton = newButton;
+    catButton.addEventListener("click", catMode);
+    catButton.addEventListener("mouseover", () => {
+      catTimer = setInterval(function () {
+        let isMove = Math.random() < 0.5;
+        if (isMove) {
+          let isRight = Math.random() < 0.5;
+          if (isRight) {
+            btnMoveR();
+          } else {
+            btnMoveL();
           }
-          // 在此处执行持续触发的操作
-          console.log("持续触发");
-        }, 500); // 100毫秒为触发间隔，您可以根据需要调整
-        
-      });
-      catButton.addEventListener('mouseleave', () => {
-        clearInterval(timer);
-        
-      });
-      catButton.addEventListener("click", () => {
-        let isRight = Math.random() < 0.5;
-        if (isRight) {
-          fallRight();
-        } else {
-          fallLeft();
         }
-      });
-    }
+        // 在此处执行持续触发的操作
+        console.log("持续触发");
+      }, 500); // 100毫秒为触发间隔，您可以根据需要调整
+    });
+    catButton.addEventListener("mouseleave", () => {
+      console.log("清除");
 
-  
-
+      clearInterval(catTimer);
+    });
+    catButton.addEventListener("click", () => {
+      let isRight = Math.random() < 0.5;
+      if (isRight) {
+        fallRight();
+      } else {
+        fallLeft();
+      }
+    });
+    catButton.addEventListener("click", () => {
+      changeColor([248, 203, 0]);
+    });
+  }
 
   popUp.style.display = "none";
 });
 modeSelect.addEventListener("change", (e) => {
   if (modeSelect.value === "person") {
     modeDisp.style.display = "block";
-  } else { 
+  } else {
     modeDisp.style.display = "none";
   }
-
 });
 function playFallAudio() {
   const audio = document.createElement("audio");
@@ -131,7 +133,6 @@ forButton.addEventListener("click", () => {
 });
 
 
-
 //* -----------------------------------------------------------------------------
 // *                     按鈕 監聽 功能結束
 //* -------------------------------------------------------------------------
@@ -142,9 +143,9 @@ forButton.addEventListener("click", () => {
 var observer = new MutationObserver(function (meritsChange) {
   meritsChange.forEach(function (meritChange) {
     if (meritChange.type === "childList" || meritChange.type === "subtree") {
-      if (Number(meritAmt.innerHTML) >= 10) {
-        shopShow();
-      }
+      // if (Number(meritAmt.innerHTML) >= 10) {
+      //   shopShow();
+      // }
       if (Number(meritAmt.innerHTML) >= 100 && !isTriple) {
         triButton.disabled = false;
       }
@@ -162,6 +163,20 @@ var observer = new MutationObserver(function (meritsChange) {
 });
 observer.observe(meritAmt, { childList: true, subtree: true });
 
+var shopObserver = new MutationObserver(function (shopMeritsChange) {
+  shopMeritsChange.forEach(function (shopMeritChange) {
+    if (
+      shopMeritChange.type === "attributes") {
+      console.log(11111);
+      shopRemind([0, 0, 0], [288, 194, 0], new Date().getTime(), 1000);
+    }
+  });
+});
+var shopNodes = [triButton, quintButton, gatlButton, forButton];
+shopNodes.forEach(function (shopNode) {
+  shopObserver.observe(shopNode, { attributes:true });
+});
+
 //* -----------------------------------------------------------------------------
 // *                     merit 監聽 功能結束
 //* -------------------------------------------------------------------------
@@ -176,6 +191,8 @@ function generate() {
   } else {
     meritAmt.innerHTML++;
   }
+      changeColor([248, 203, 0]);
+
 }
 function carnivore() {
   var timeId = window.setInterval(
@@ -207,6 +224,8 @@ function catMode() {
     }
   }
 }
+
+
 //* -----------------------------------------------------------------------------
 // *                     merit 按鈕變化 功能結束
 //* -------------------------------------------------------------------------
@@ -260,20 +279,6 @@ function meritForever() {
 //* -----------------------------------------------------------------------------
 // *                     動畫 控制
 //* -------------------------------------------------------------------------
-// function animate({ timing, draw, duration }) {
-//   let startTime = performance.now();
-//   requestAnimationFrame((time) => {
-//     let timeFraction = (time - startTime) / duration;
-//     if (timeFraction > 1) {
-//       timeFraction = 1;
-//     }
-//     let progress = timing(timeFraction);
-//     draw(progress);
-//     if (timeFraction < 1) {
-//       requestAnimationFrame(animate);
-//     }
-//   })
-// }
 function animate({ timing, draw, duration }) {
   let start = performance.now();
 
@@ -410,43 +415,134 @@ function btnMoveR() {
   let endR = clickArea.offsetLeft + clickArea.offsetWidth;
   // console.log(catButton.style.left);
   animate({
-    duration: 4000,
+    duration: 1000,
     timing: makeEaseOut(quad),
     draw: function (progress) {
-      if (startR< endR) {
+      if (startR < endR) {
         catButton.style.left =
           startR + progress * 200 + catButton.offsetWidth > endR
             ? endR - catButton.offsetWidth - progress * 200 + "px"
             : startR + progress * 200 + "px";
-  // console.log(catButton.style.left);
-        
+        // console.log(catButton.style.left);
       } else {
-        catButton.style.left = endR - catButton.offsetWidth - progress * 200 + "px";
-      }  
-      
-    }
-})
+        catButton.style.left =
+          endR - catButton.offsetWidth - progress * 200 + "px";
+      }
+    },
+  });
 }
 function btnMoveL() {
   let startL = catButton.offsetLeft;
   let endL = clickArea.offsetLeft;
   animate({
-    duration: 1000,
+    duration: 500,
     timing: makeEaseOut(quad),
     draw: function (progress) {
       if (startL > endL) {
         catButton.style.left =
           startL - progress * 200 > endL
-            ?
-            startL - progress * 200 + "px" :
-            -(startL - progress * 200) + "px";
+            ? startL - progress * 200 + "px"
+            : -(startL - progress * 200) + "px";
       } else {
         catButton.style.left = -(startL - progress * 200) + "px";
       }
     },
   });
-  
 }
+function stopShopRemind() {
+  cancelAnimationFrame(animationFrameId);
+}
+var animationFrameId;
+function shopRemind(
+  currentColor,
+  targetColor,
+  startTime,
+  totalTime,
+  isFirst = true,
+  startColor = null
+) {
+  
+  if (
+
+      triButton.disabled&& 
+      quintButton.disabled&&
+      gatlButton.disabled&&
+      forButton.disabled
+
+  ) {
+    cancelAnimationFrame(animationFrameId);
+    return;
+  }
+
+   startColor = isFirst ? currentColor : startColor;
+  
+  let currTime = new Date().getTime();
+  let elapsedTime = currTime - startTime;
+
+  let ratio = Math.min(elapsedTime / totalTime, 1);
+  let nextColor = [];
+  for (let i = 0; i < 3; i++) {
+    nextColor[i] = currentColor[i] + (targetColor[i] - currentColor[i]) * ratio;
+  }
+  shopTag.style.color = `rgb(${Math.round(nextColor[0])},
+  ${Math.round(nextColor[1])},
+  ${Math.round(nextColor[2])})`;
+  console.log(shopTag.style.color);
+  // 未達到目標顏色前繼續變化
+  if (elapsedTime < totalTime) {
+    
+  animationFrameId =requestAnimationFrame(() => {
+      shopRemind(nextColor, targetColor, startTime, totalTime,false,startColor); //注意要用nextColor不然不變化
+    });
+  } else {
+    //完成變化 反覆變化
+    temp = startColor;
+    startColor = targetColor;
+    targetColor = temp;
+
+    startTime = new Date().getTime();
+    animationFrameId = requestAnimationFrame(() => {
+      shopRemind(
+        nextColor,
+        targetColor,
+        startTime,
+        totalTime,
+        false,
+        startColor
+      ); //注意要用nextColor不然不變化
+    });
+    
+  }
+}
+function changeColor(targetColorArr) {
+  // 获取颜色字符串
+
+  let colorString = window.getComputedStyle(meritAmt).color;
+  // 使用正则表达式匹配 RGB 值
+  let rgbRegex = /rgb\((\d+),\s*(\d+),\s*(\d+)\)/;
+  let matches = colorString.match(rgbRegex); //返回字串第一個為原字串，後續為所有匹配字串
+  colorArr = matches.slice(1).map(Number); //  匹配之數值數組
+console.log(colorArr);
+  if (colorArr.every((value, index) => value === targetColorArr[index])) {
+    return;
+  }
+  
+  let ratio = parseInt(meritAmt.innerHTML) / 1000;
+  let nextColorArr = [];
+  if (ratio > 0) {
+    for (let i = 0; i < colorArr.length; i++) {
+       nextColorArr[i]=targetColorArr[i]*ratio;
+      
+    }
+    
+  }
+  meritAmt.style.color = `rgb(${Math.round(nextColorArr[0])},
+  ${Math.round(nextColorArr[1])},
+  ${Math.round(nextColorArr[2])})`;
+}
+
+  
+
 
 //* -----------------------------------------------------------------------------
 // *                     動畫 控制結束
@@ -454,7 +550,3 @@ function btnMoveL() {
 //* -----------------------------------------------------------------------------
 // *                     商店彈窗 控制
 //* -------------------------------------------------------------------------
-
-
-
-
